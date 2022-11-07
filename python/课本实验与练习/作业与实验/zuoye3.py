a71b4f94f7a604dfa83d1a7f1902d896d0ee1b55
小邢不枫 <577023109@qq.com>
2022-11-07 16:38:11 +0800

    python

--- python/课本实验与练习/作业与实验/zuoye3.py
+++ python/课本实验与练习/作业与实验/zuoye3.py
@@ -1,19 +1,37 @@
-#编写程序，统计字符串中字母、数字、汉字、其他字符的个数
 import string
+import re
 
-lst = "before i meet you,my life is negligible. 遇到你之前，我的生活不值一提.i love you 3000 times，我爱你3000遍 你好"
-count_yw = count_num = count_zw = count_fh = 0
-for s in lst:
-    if s in string.ascii_letters:
-        count_yw += 1
-    elif s.isdigit():
-        count_num += 1
-    elif s.isalpha():
-        count_zw += 1
+# 编写程序，统计字符串中字母、数字、汉字、其他字符的个数
+# lst = "before i meet you,my life is negligible. 遇到你之前，我的生活不值一提.i love you 3000 times，我爱你3000遍 你好"
+# count_yw = count_num = count_zw = count_fh = 0
+# for s in lst:
+#     if s in string.ascii_letters:#所有在ascii表中的字母不包括汉字
+#         count_yw += 1
+#     elif s.isdigit():#是否由数字组成
+#         count_num += 1
+#     elif s.isalpha():#是否由字母组成（包括中文）
+#         count_zw += 1
+#     else:
+#         count_fh += 1
+# print('英文字符：', count_yw)
+# print('数字：', count_num)
+# print('中文：', count_zw)
+# print('特殊字符：', count_fh)
+
+# 编写程序，统计字符串词个数，词存放在字典中，由词分隔的字符串统一计算其他。
+
+
+str = "Keep your friends close,but your enemies closer."
+lst = re.findall(r'\b\w.+?\b', str)
+print(lst)
+splitNum = len(lst)
+dict1 = {}
+for i in range(0, splitNum):
+    str1 = lst[i]
+    if str1 in dict1:#如果当前字典中已经存在该元素，将该元素弹出
+        n = dict1.pop(str1) #他的字典中pop()方法返回值是被删掉的元素值
+        dict1[str1] = n + 1
     else:
-        count_fh += 1
-print('英文字符：', count_yw)
-print('数字：', count_num)
-print('中文：', count_zw)
-print('特殊字符：', count_fh)
-
+        dict1[str1] = 1#将所有元素值都赋值1
+print(dict1)
+print("分隔符个数：", splitNum)
---
+++ python/课本实验与练习/作业与实验/zuoye4.py
@@ -0,0 +1,32 @@
+# 编写函数，求两数的做大公约数和最小公倍数
+# ① 整数A对整数B进行取整, 余数用整数C来表示 举例: C = A % B
+# ② 如果C等于0,则C就是整数A和整数B的最大公约数
+# ③ 如果C不等于0, 将B赋值给A, 将C赋值给B ,然后进行 1, 2 两步,直到余数为0, 则可以得知最大公约数
+# 最小公倍数 = 两个整数的乘积 / 最大公约数
+# def GCDandLCM(a, b):
+#     gcd = 1
+#     lcm = a * b
+#     while b != 0:
+#         yushu = a % b
+#         a = b
+#         b = yushu
+#     else:
+#         gcd = a
+#     lcm = lcm / gcd
+#     return gcd, lcm
+#
+#
+# a, b = map(int, input("请输入a,b:").split())
+# num1, num2 = GCDandLCM(a, b)
+# print("最大公约数{0}，最小公倍数{1}".format(num1, num2))
+# 编写函数，可以接收任意多个整数并用字典输出最大值、最小值、平均值、和所有整数之和，对应的关键字”max“ ”min“ ”avg“ ”sum“
+def dictNum(lst):
+    dict0 = {"max":max(lst),"min":min(lst),"avg":sum(lst)/len(lst),"sum":sum(lst)}
+    return dict0
+lst = list(map(int,input("请输入数组:").split()))
+print(lst)
+dict1 = dictNum(lst)
+print(dict1)
+
+
+
